ae3fbbac48601387d8f87fd2a9e5d7215724dd74
Robert Bond <robert@bryx.com>
2018-03-11 23:36:55 -0400

    WIP: Start transitioning to splitting the weapons list client side by weapon type

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -3,13 +3,28 @@ import * as XHR from "i18next-xhr-backend";
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import {Loader, Message} from "semantic-ui-react";
-import {Weapon} from "./models/weapon";
+import {Weapon, WeaponType} from "./models/weapon";
 import {MHWApi} from "./utils/mhwApi";
 import Options = i18n.InitOptions;
 
 interface MainState {
     status: { key: "ready" } | { key: "error", message: string } | { key: "loading" };
-    weapons: Weapon[];
+    weapons: {
+        greatSword: Weapon[],
+        longSword: Weapon[],
+        swordAndShield: Weapon[],
+        dualBlades: Weapon[],
+        hammer: Weapon[],
+        huntingHorn: Weapon[],
+        lance: Weapon[],
+        gunlance: Weapon[],
+        switchAxe: Weapon[],
+        chargeBlade: Weapon[],
+        insectGlaive: Weapon[],
+        lightBowgun: Weapon[],
+        heavyBowgun: Weapon[],
+        bow: Weapon[],
+    };
 }
 
 class Main extends React.Component<{}, MainState> {
@@ -32,7 +47,22 @@ class Main extends React.Component<{}, MainState> {
 
         this.state = {
             status: { key: "ready" },
-            weapons: [],
+            weapons: {
+                greatSword: [],
+                longSword: [],
+                swordAndShield: [],
+                dualBlades: [],
+                hammer: [],
+                huntingHorn: [],
+                lance: [],
+                gunlance: [],
+                switchAxe: [],
+                chargeBlade: [],
+                insectGlaive: [],
+                lightBowgun: [],
+                heavyBowgun: [],
+                bow: [],
+            },
         };
     }
 
@@ -45,7 +75,22 @@ class Main extends React.Component<{}, MainState> {
             if (result.success == true) {
                 this.setState({
                     status: { key: "ready" },
-                    weapons: result.value,
+                    weapons: {
+                        greatSword: result.value.filter(w => WeaponType[w.type] == "great-sword"),
+                        longSword: [],
+                        swordAndShield: [],
+                        dualBlades: [],
+                        hammer: [],
+                        huntingHorn: [],
+                        lance: [],
+                        gunlance: [],
+                        switchAxe: [],
+                        chargeBlade: [],
+                        insectGlaive: [],
+                        lightBowgun: [],
+                        heavyBowgun: [],
+                        bow: [],
+                    },
                 });
             } else {
                 console.log("Failed to grab weapons");
