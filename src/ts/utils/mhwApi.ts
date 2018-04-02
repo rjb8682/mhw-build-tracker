6b9c9b6c42f283411fdba3ca240af6cafec33f1e
Robert Bond <robert@bryx.com>
2018-04-02 12:19:36 -0400

    Added missing endpoint functions

--- src/ts/utils/mhwApi.ts
+++ src/ts/utils/mhwApi.ts
@@ -2,6 +2,11 @@ import {ParseResult, ParseUtils} from "bryx-cereal";
 import {ClientConfig, HttpClient, HttpRequest, HttpResponse, ResponseStatus} from "bryx-spoon";
 import * as i18n from "i18next";
 import {Weapon} from "../models/weapon";
+import {Armor, ArmorSet} from "../models/armor";
+import {Charm} from "../models/charm";
+import {Decoration} from "../models/decoration";
+import {Item} from "../models/item";
+import {Skill} from "../models/skill";
 
 export type ApiResult<T> =
     { success: true, value: T } |
@@ -56,11 +61,59 @@ export class MHWApi {
         return new HttpClient(httpClientConfig);
     })();
 
-    public static getWeapons(idOrSlug: string | null, callback: (result: ApiResult<Weapon[]>) => void): void {
+    public static getWeapons(idOrSlug: number | string | null, callback: (result: ApiResult<Weapon[]>) => void): void {
         const wrappedCallback = (request: HttpRequest, response: HttpResponse) => {
             callback(apiArrayResultFromParse(response, Weapon.parse, "warn"));
         };
 
         MHWApi.http.get(`/weapons${idOrSlug != null ? `/${idOrSlug}` : ""}`, null, wrappedCallback);
+    }
+
+    public static getArmor(idOrSlug: number | string | null, callback: (result: ApiResult<Armor[]>) => void): void {
+        const wrappedCallback = (request: HttpRequest, response: HttpResponse) => {
+            callback(apiArrayResultFromParse(response, Armor.parse, "warn"));
+        };
+
+        MHWApi.http.get(`/armor${idOrSlug != null ? `/${idOrSlug}` : ""}`, null, wrappedCallback);
+    }
+
+    public static getArmorSets(id: number, callback: (result: ApiResult<ArmorSet[]>) => void): void {
+        const wrappedCallback = (request: HttpRequest, response: HttpResponse) => {
+            callback(apiArrayResultFromParse(response, ArmorSet.parse, "warn"));
+        };
+
+        MHWApi.http.get(`/armor/sets${id != null ? `/${id}` : ""}`, null, wrappedCallback);
+    }
+
+    public static getCharms(idOrSlug: number | string | null, callback: (result: ApiResult<Charm[]>) => void): void {
+        const wrappedCallback = (request: HttpRequest, response: HttpResponse) => {
+            callback(apiArrayResultFromParse(response, Charm.parse, "warn"));
+        };
+
+        MHWApi.http.get(`/charms${idOrSlug != null ? `/${idOrSlug}` : ""}`, null, wrappedCallback);
+    }
+
+    public static getDecorations(idOrSlug: number | string | null, callback: (result: ApiResult<Decoration[]>) => void): void {
+        const wrappedCallback = (request: HttpRequest, response: HttpResponse) => {
+            callback(apiArrayResultFromParse(response, Decoration.parse, "warn"));
+        };
+
+        MHWApi.http.get(`/decorations${idOrSlug != null ? `/${idOrSlug}` : ""}`, null, wrappedCallback);
+    }
+
+    public static getItems(id: number | null, callback: (result: ApiResult<Item[]>) => void): void {
+        const wrappedCallback = (request: HttpRequest, response: HttpResponse) => {
+            callback(apiArrayResultFromParse(response, Item.parse, "warn"));
+        };
+
+        MHWApi.http.get(`/item${id != null ? `/${id}` : ""}`, null, wrappedCallback);
+    }
+
+    public static getSkills(idOrSlug: number | string | null, callback: (result: ApiResult<Skill[]>) => void): void {
+        const wrappedCallback = (request: HttpRequest, response: HttpResponse) => {
+            callback(apiArrayResultFromParse(response, Skill.parse, "warn"));
+        };
+
+        MHWApi.http.get(`/skills${idOrSlug != null ? `/${idOrSlug}` : ""}`, null, wrappedCallback);
     }
 }
